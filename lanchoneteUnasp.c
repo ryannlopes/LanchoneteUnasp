d20e0ab64d55c33066b61223be15962b7af4e330
Guilherme Gomes <73298656+guilhermegomees@users.noreply.github.com>
2021-11-03 18:38:11 -0300

    add desconto, retirada e entrega no pedido

--- lanchoneteUnasp.c
+++ lanchoneteUnasp.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void condicoesCompra();
+
 int main(){
 
-    int cod, qnt, novoPedido, novoLanche, codInvalido, qntInvalida;
-    char nome[20];
-    float saldoDia=0, totalPedido;
+    int cod, qnt, novoPedido, novoLanche, codInvalido, qntInvalida, quantPedidos, opcaoEntRet;
+    char nome[20], end[30];
+    float saldoDia=0, totalPedido, taxaEntrega = 7.50, desconto;
 
     do{
         //COMEÇANDO UM NOVO PEDIDO
         system("cls");
         printf("NOVO PEDIDO\n\n");
 
-        totalPedido=0;//Toda vez que estiver um novo pedido ele será zerado
+        totalPedido = 0;//Toda vez que estiver um novo pedido ele será zerado
+        quantPedidos = 0;
+        opcaoEntRet = 0;
         
         fflush(stdin); //Limpando o buton do teclado
 
@@ -27,9 +31,9 @@ int main(){
             //cardapio
             do{
                 printf("                      LANCHES                         |                       BEBIDAS                   \n");
-                printf("                                                      |                                                \n");
+                printf("                                                      |                                                 \n");
                 printf("  CODIGO        NOME                      PRECO       |  CODIGO        NOME                        PRECO\n");
-                printf("                                                      |                                                \n");
+                printf("                                                      |                                                 \n");
                 printf("  1             X-Burguer                 R$ 6.99     |  11            Coca-Cola 2L                R$ 12.00\n");
                 printf("  2             X-Salada                  R$ 7.50     |  12            Guarana Antarctica 2L       R$ 9.00\n");
                 printf("  3             X-Bacon                   R$ 9.00     |  13            Sprite 2L                   R$ 9.00\n");
@@ -72,25 +76,25 @@ int main(){
             }while(qntInvalida==1);
 
             //Calculado o valor de cada lanche com a quantidade
-            switch(cod) {
-                case 1: totalPedido = totalPedido + (6.99*qnt); break;
-                case 2: totalPedido = totalPedido + (7.50*qnt); break;
-                case 3: totalPedido = totalPedido + (9.00*qnt); break;
-                case 4: totalPedido = totalPedido + (10.00*qnt); break;
-                case 5: totalPedido = totalPedido + (12.00*qnt); break;
-                case 6: totalPedido = totalPedido + (15.00*qnt); break;
-                case 7: totalPedido = totalPedido + (5.50*qnt); break;
-                case 8: totalPedido = totalPedido + (5.50*qnt); break;
-                case 9: totalPedido = totalPedido + (5.50*qnt); break;
-                case 10: totalPedido = totalPedido + (6.00*qnt); break;
-                case 11: totalPedido = totalPedido + (12.00*qnt); break;
-                case 12: totalPedido = totalPedido + (9.00*qnt); break;
-                case 13: totalPedido = totalPedido + (9.00*qnt); break;
-                case 14: totalPedido = totalPedido + (8.50*qnt); break;
-                case 15: totalPedido = totalPedido + (3.50*qnt); break;
-                case 16: totalPedido = totalPedido + (3.50*qnt); break;
-                case 17: totalPedido = totalPedido + (3.50*qnt); break;
-                case 18: totalPedido = totalPedido + (5.00*qnt); break;
+            switch(cod){
+                case 1: totalPedido = totalPedido + (6.99*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 2: totalPedido = totalPedido + (7.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 3: totalPedido = totalPedido + (9.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 4: totalPedido = totalPedido + (10.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 5: totalPedido = totalPedido + (12.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 6: totalPedido = totalPedido + (15.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 7: totalPedido = totalPedido + (5.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 8: totalPedido = totalPedido + (5.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 9: totalPedido = totalPedido + (5.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 10: totalPedido = totalPedido + (6.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 11: totalPedido = totalPedido + (12.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 12: totalPedido = totalPedido + (9.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 13: totalPedido = totalPedido + (9.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 14: totalPedido = totalPedido + (8.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 15: totalPedido = totalPedido + (3.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 16: totalPedido = totalPedido + (3.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 17: totalPedido = totalPedido + (3.50*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
+                case 18: totalPedido = totalPedido + (5.00*qnt); quantPedidos = (quantPedidos + 1) * qnt; break;
             }
 
             //Perguntado se vai add mais produtos
@@ -110,16 +114,67 @@ int main(){
             //Fechamento do pedido
             }while(novoLanche != 0 && novoLanche!=1);
 
-            printf("\n");
-
         }while(novoLanche==1);
+
+        system("cls");
+        if(quantPedidos >= 5)
+        {
+            printf("PARABENS!!!\nPOR PEDIR 5 OU MAIS UNIDADES, VOCE GANHOU 10%% DE DESCONTO!!\n\n");
+            system("pause");
+            system("cls");
+        }
+
+        do
+        {
+            printf("Entrega ou Retirada?\n\n");
+            printf("1 - Entrega\n");
+            printf("0 - Retirada\n\n");
+
+            printf("Opcao: ");
+            scanf("%d", &opcaoEntRet);
+
+            //Validação da opção
+            if(opcaoEntRet!=0 && opcaoEntRet!=1){
+                printf("\nOpcao invalida:\n\n");
+            }
+
+            if(opcaoEntRet == 1)
+            {   
+                system("cls");
+                printf("Informe o endereco: ");
+                fflush(stdin);
+                scanf("%[^\n]s", end);
+            }
+            
+        }while(opcaoEntRet != 0 && opcaoEntRet!=1);
+
 
         //imprimindo Recibo
         system("cls");
-        printf("RECIBO\n");
+        printf("______RECIBO______\n\n");
         printf("Nome do Cliente: %s\n", nome);
+        if(opcaoEntRet == 1)
+        {
+            printf("Endereco: %s\n", end);
+        }
         printf("Total a pagar: R$ %.2f\n", totalPedido);
 
+        if(quantPedidos >= 5)
+        {   
+            desconto = totalPedido * 0.1;
+            totalPedido = totalPedido - desconto;
+            printf("Total a pagar com desconto: R$ %.2f\n", totalPedido);
+        }
+
+        if(opcaoEntRet == 1)
+        {
+            totalPedido = totalPedido + taxaEntrega;
+            printf("Total a pagar com a taxa de entrega: R$ %.2f\n", totalPedido);
+        }
+        printf("\n");
+        system("pause");
+        
+            
         //Saldo do dia
         saldoDia += totalPedido;
 
@@ -127,7 +182,8 @@ int main(){
 
         //Pergunta se vai ter um novo pedido ou vai fechar o dia.
         do{
-            printf("O que deseja fazer?\n");
+            system("cls");
+            printf("O que deseja fazer?\n\n");
             printf("1 - Atender novo cliente\n");
             printf("0 - Sair do Sistema\n\n");
 
@@ -147,4 +203,8 @@ int main(){
     printf("\n");
 
     return 0;
+}
+
+void condicoesCompra(){
+    
 }
