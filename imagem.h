51827ec594dd154ac92654415a1590ad0ee15ea8
Ryan Nicolau Lopes <ryan_nlopes@outlook.com>
2021-11-16 16:23:21 -0300

    Atualizando

---
+++ imagem.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int imagem(){
+    printf("**************** BEM-VINDO ***************");
+    printf("\nPressione ENTER para iniciar o sistema...");
+    printf("                                                  \n");
+    printf("                  ````........``                  \n");
+    printf("           `..-:::::::::/+++++++///:-.`           \n");
+    printf("        .-:::////////////+++++++++++++//:.`       \n");
+    printf("     `-::////////////////+++++++++++++++++/-`    \n");
+    printf("    .:///////////////////+++++++++++++++++++/.    \n");
+    printf("   `:////////////////////+++++++++++++++++++++.   \n");
+    printf("   -/////////////////////+++++++++++++++++++++/   \n");
+    printf("   -/////////////////////+++++++++++++++++++++:   \n");
+    printf("   `ossssssssssssssssssssyyyyyyyyyyyyyyyyyyyyy`   \n");
+    printf("  /yhhhhhhhhhhhhhhhhhhhhhdddyyyyyddhyyyyydddddh/  \n");
+    printf(" -hhhhhhhhhhhhhhhhhhhhhhhdddhyyyhdddyyyyhddddddd- \n");
+    printf(" :hhhhhhhhhhhhhhhhhhhhhhhhdddddddhhddhhdddhhdddd: \n");
+    printf("  sso++++oyyso++++oyyso++++oyhyo++++oyhys++++oys` \n");
+    printf("./++oossoo+++oossoo+++oossoo+++oossoo+++oossoo++/.\n");
+    printf("+oosssyysssssssyysssssssyyyssssssyyyssssssyyyssoo+\n");
+    printf("`+yyhhhhhhyyyhhhhhhyyyhhhddhhhhhddddhhhhhddddhyy+.\n");
+    printf(" -hhhhhhhhhhhhhhhhhhhhhhhddddddddddddddddddddddd: \n");
+    printf("  ohhhhhhhhhhhhhhhhhhhhhhddddddddddddddddddddddo` \n");
+    printf("   -+++++++++++++++++++++ooooooooooooooooooooo-`  \n");
+    printf("   .::::-.....-:-.....-:://///////////////////-   \n");
+    printf("   ://///-....://-...-///+++++++++++++++++++++/   \n");
+    printf("   ://///-....://:::::///+++++++++++++++++++++/   \n");
+    printf("   :::::::-..-::::::::::/+++++++++++++++++++++/   \n");
+    printf("   `..........................................`   \n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    getchar();
+}
